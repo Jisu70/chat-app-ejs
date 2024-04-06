941129287d918a40a2fed51003eb9465924cb551
Jisu70 <sudiptajana70@gmail.com>
2024-04-06 23:54:35 +0530

    login functionality completed

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,3 +1,12 @@
+// Npm modules
+const bcrypt = require('bcrypt') ;
+const jwt = require("jsonwebtoken");
+const createError = require("http-errors");
+
+// Import Model
+const People = require('../models/People')
+
+
 const getLogin = (req, res) => {
     try {
         res.render('index', {
@@ -9,20 +18,74 @@ const getLogin = (req, res) => {
 }
 
 // save user 
-const saveUser = (req, res) => {
-    console.log("Got clicked");
+const loginUser = async (req, res) => {
     try {
+        const { emailOrMobile, password } = req.body ;
+
+        if (!emailOrMobile || !password) {
+            return res.status(400).json({
+                status: "error",
+                message: "Email or mobile and password must be provided."
+            });
+        }
         
+        const isUser = await People.findOne({ 
+            $or: [{ mobile : emailOrMobile }, { email : emailOrMobile}],
+        }) ;
+        if (!isUser) {
+            return res.status(400).json({
+                status : "error",
+                message : "No user found with this email or mobile."
+            });
+        }
+        const isMatched = await bcrypt.compare(password, isUser.password );
+        if (!isMatched) {
+            return res.status(400).json({
+                status : "error",
+                message : "Wrong password."
+            });
+        };
+        const {name , email, mobile} = isUser ;
+        const userObject = {
+            name,
+            email,
+            mobile
+        }
+
+        // generate token
+        const token = jwt.sign(userObject, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRY,
+        });
+
+        // Now storing this jwt token in cookie
+        res.cookie(process.env.COOKIE_NAME,token,{
+            maxAge: process.env.JWT_EXPIRY,
+            httpOnly: true,
+            signed: true,
+        });
+
+        // Redirect to inbox page
+        res.render("inbox",{
+            loggedInUser : userObject,
+            title : "Inbox page"
+        });
     } catch (error) {
         console.log(error);
-        res.status(500).json({
-            status : "error",
-            message : "there was a problem to handle your request"
-        })
+        res.render("index", {
+            data: {
+                emailOrMobile
+            },
+            errors: {
+              common: {
+                msg: error.message,
+              },
+            },
+        });
     }
 }
 
 
 module.exports = {
-    getLogin
+    getLogin,
+    loginUser
 }
--- routers/loginRouter.js
+++ routers/loginRouter.js
@@ -1,8 +1,12 @@
 // Importing express
 const express = require('express');
 const router = express.Router() ;
-const { getLogin} = require('../controllers/loginController.js')
+const { getLogin, loginUser} = require('../controllers/loginController.js')
 
+// Render login Page
 router.get('/', getLogin ) ;
+
+// Hnadle login Functionality
+router.post('/login', loginUser ) ;
 
 module.exports = router ;
