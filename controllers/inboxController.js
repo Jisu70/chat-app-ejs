166603c595fb73a91af58a64743e999d11d72880
Jisu70 <sudiptajana70@gmail.com>
2024-06-09 21:27:22 +0530

    fixed css and  hide the message box by default

--- controllers/inboxController.js
+++ controllers/inboxController.js
@@ -3,23 +3,23 @@ const Conversation = require('../models/Converstion') ;
 const People = require('../models/People') ;
 
 
-// 
+// Inbox page 
 const getInbox = async (req, res) => {
     try {
         const user_id = req.user._id ; // Logged in user ID 
         const all_conversation = await Conversation.find({'creator': user_id }, 'participant last_updated')
         .populate({
             path: 'participant',
-            select: 'name avatar' 
+            select: ['name', 'avatar'] 
         })
         .sort({ last_updated: -1 });
-        console.log("all_conversation", all_conversation);
-        return res.send(all_conversation)
+        console.log(all_conversation);
         res.render('inbox', {
             all_conversation
         })
     } catch (error) {
-        console.log(error)
+        console.log(error) ;
+        res.status(500).send('Internal Server Error');
     }
 }
 
@@ -27,17 +27,32 @@ const getInbox = async (req, res) => {
 const crerateConversation = async (req, res) => {
     try {
         const { id } = req.body;
+        const haveConversation = await Conversation.findOne({
+            $or: [
+                { creator: req.user._id, participant: id },
+                { creator: id, participant: req.user._id }
+            ]
+        });     
+        if (haveConversation) {
+            return res.status(400).json({
+                errors: {
+                  common: {
+                    msg: "Already have conversation.",
+                  },
+                },
+            });
+        }   
         const creat_conversation = new Conversation({
             creator: req.user._id,
             participant:  id 
         
         })
 
-        const save_conversation = await creat_conversation.save();
+        await creat_conversation.save();
 
         res.status(200).json({
             message: "Conversation was added successfully!",
-          });
+        });
       
         
     } catch (error) {
@@ -55,7 +70,7 @@ const crerateConversation = async (req, res) => {
 // Get message of the selected conversation 
 const getMessage = async (req, res) => {
     try {
-        const { conversation_id, name} = req.body ;
+        const { conversation_id } = req.body ;
         // const get_conversation_message = await 
     } catch (error) {
         console.log(error);
